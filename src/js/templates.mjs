9328678360d0f73b6e7c72eef1716a573318cfaa
Emilie Mickelsen <mickelsen.emilie@gmail.com>
2024-10-15 11:41:22 -0600

    formatted hero subtitle

--- src/js/templates.mjs
+++ src/js/templates.mjs
@@ -2,8 +2,8 @@
 export function parkInfoTemplate(info) {
     return `<a href="/" class="hero-banner__title">${info.name}</a>
       <p class="hero-banner__subtitle">
-        <span>${info.designation}</span>
-        <span>${info.states}</span>
+        <span>${info.designation}</span> <br>
+        <span>${info.states.split(',').join(', ')}</span>
       </p>`;
 }
 
