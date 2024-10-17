588c970cfcef564e60c467615ebf6357386c2769
Emilie Mickelsen <mickelsen.emilie@gmail.com>
2024-10-17 10:37:01 -0600

    finished the alerts

--- src/js/conditions.js
+++ src/js/conditions.js
@@ -1,18 +1,58 @@
-import { getParkData } from "./parkService.mjs";
+// import {
+//     getParkData,
+//     getParkAlerts,
+//     getParkVisitorCenters
+//   } from "./parkService.mjs";
+//   import {
+//     activityListTemplate,
+//     alertTemplate,
+//     visitorCenterTemplate
+//   } from "./templates.mjs";
+//   import setHeaderFooter from "./setHeaderFooter.mjs";
+import { getParkData, getParkAlerts } from "./parkService.mjs";
+import { alertTemplate } from "./templates.mjs";
 import setHeaderFooter from "./setHeaderFooter.mjs";
 
-
-function setAlerts(data) {
-    const alertsInfo = document.querySelector(".alerts");
-    const html = data.map(alertsTemplate);
-    alertsInfo.insertAdjacentElement("afterbegin", html.join(""));
+// function setAlerts(data) {
+//     const alertsInfo = document.querySelector(".alerts > ul");
+//     const html = data.map(alertTemplate);
+//     alertsInfo.insertAdjacentElement("afterbegin", html.join(""));
+//   }
+function setAlerts(alerts) {
+    const alertsContainer = document.querySelector(".alerts > ul");
+    alertsContainer.innerHTML = "";
+    const html = alerts.map(alertTemplate);
+    alertsContainer.insertAdjacentHTML("beforeend", html.join(""));
   }
 
-async function init() {
-  const parkData = await getParkData();
-  setHeaderFooter(parkData);
-
-  setAlerts(parkData);
+function setVisitorCenters(centers) {
+  const centersContainer = document.querySelector(".visitor ul");
+  const html = centers.map(visitorCenterTemplate);
+  centersContainer.insertAdjacentHTML("afterbegin", html.join(""));
 }
 
-init();
+function setActivities(activities) {
+  const activitiesContainer = document.querySelector(".activities ul");
+  const html = activityListTemplate(activities);
+  activitiesContainer.insertAdjacentHTML("afterbegin", html);
+}
+// async function init() {
+//   const parkData = await getParkData();
+//   setHeaderFooter(parkData);
+//   const alerts = await getParkAlerts(parkData.parkCode);
+//   const visitorCenters = await getParkVisitorCenters(parkData.parkCode);
+//   setHeaderFooter(parkData);
+//   setAlerts(alerts);
+//   setVisitorCenters(visitorCenters);
+//   setActivities(parkData.activities);
+// }
+
+// init();
+async function init() {
+    const parkData = await getParkData();
+    const alerts = await getParkAlerts(parkData.parkCode);
+    setHeaderFooter(parkData);
+    setAlerts(alerts);
+  }
+  
+  init();
--- src/js/templates.mjs
+++ src/js/templates.mjs
@@ -37,25 +37,43 @@ export function footerTemplate(info) {
 }
 
 // alerts template
+// export function alertTemplate(alert) {
+//   let alertCategory = "";
+//   switch (alert.catergory) {
+//     case "Park Closure":
+//       alertCategory = "closure";
+//       break;
+//     default:
+//       alertCategory = alert.catergory.toLowerCase();
+//   }
+//   return `<li class = "alert">
+//   <svg class="icon" focusable="false" aria-hidden="true"><use xmlns:xlink="http://www.w3.org/1999/xlink" xlink:href="/images/sprite.symbol.svg#alert-${alertCategory}"></use></svg>
+//   <span>
+//     <h3 class="alert-${alertCategory}">${alert.title}</h3>
+//     <p>${alert.description}</p>
+//   </span></li>`;
+// }
 export function alertTemplate(alert) {
-  let alertCategory = "";
-  switch (alert.catergory) {
+  let alertType = "";
+  // most of the alerts are one word and line up with the icons nicely. "Park Closure" is the exception
+  switch (alert.category) {
     case "Park Closure":
-      alertCategory = "closure";
+      alertType = "closure";
       break;
     default:
-      alertCategory = alert.catergory.toLowerCase();
+      alertType = alert.category.toLowerCase();
   }
-  return `<li class = alert">
+  return `<li class="alert">
   <svg class="icon" focusable="false" aria-hidden="true"><use xmlns:xlink="http://www.w3.org/1999/xlink" xlink:href="/images/sprite.symbol.svg#alert-${alertType}"></use></svg>
   <div>
-    <h3 class="alert-${alertCategory}">${alert.title}</h3>
+    <h3 class="alert-${alertType}">${alert.title}</h3>
     <p>${alert.description}</p>
   </div></li>`;
 }
+
 // visitors center template
 export function visitorCenterTemplate(center) {
-  return `<li class="visitor-center">
+  return `<li class="visitor">
   <h4>${center.name}</h4>
   <p>${center.description}</p>
   <p>${center.directionsInfo}</p>
