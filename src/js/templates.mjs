e9381a8588bfe11ea7df0247a6fb567108f409bb
Emilie Mickelsen <mickelsen.emilie@gmail.com>
2024-10-17 15:39:49 -0600

    trying to fix render icons

--- src/js/templates.mjs
+++ src/js/templates.mjs
@@ -36,6 +36,8 @@ export function footerTemplate(info) {
   </section>`;
 }
 
+import spritePath from '../images/sprite.symbol.svg';
+
 export function alertTemplate(alert) {
   let alertType = "";
   // most of the alerts are one word and line up with the icons nicely. "Park Closure" is the exception
@@ -47,7 +49,8 @@ export function alertTemplate(alert) {
       alertType = alert.category.toLowerCase();
   }
   return `<li class="alert">
-  <svg class="icon" focusable="false" aria-hidden="true"><use xmlns:xlink="http://www.w3.org/1999/xlink" xlink:href="/images/sprite.symbol.svg#alert-${alertType}"></use></svg>
+  <svg class="icon" focusable="false" aria-hidden="true">
+  <use href="${spritePath}#alert-${alertType}"></use></svg>
   <div>
     <h3 class="alert-${alertType}">${alert.title}</h3>
     <p>${alert.description}</p>
