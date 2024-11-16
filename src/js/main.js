23782207c9171fb123452bbef2719cb88c58a519
Emilie Mickelsen <mickelsen.emilie@gmail.com>
2024-11-15 18:52:05 -0700

    nps 6 progress

--- src/js/main.js
+++ src/js/main.js
@@ -17,30 +17,56 @@ function setParkInfoLinks(data) {
   infoEl.insertAdjacentHTML("afterbegin", html.join(""));
 }
 
-function enableNavigation() {
-  const menuButton = document.querySelector("#global-nav-toggle");
-  
-  // when the main menu button is clicked:
-  menuButton.addEventListener("click", (ev) => {
-    let target = ev.target;
-  // toggle the show class on the global-nav
-  document.querySelector(".global-nav").classList.toggle("show");
-  // check to see if ev.target is the button or something inside the button
-  if (target.tagName != "BUTTON") {
-    target = target.closest("button");
-  }
-  // check to see if we just opened or closed the menu
-  if (document.querySelector(".global-nav").classList.contains("show")) {
-    // if we opened it then set the aria-expanded attribute to true
-    target.setAttribute("aria-expanded", true);
-  } else {
-    // if we closed it then set the aria-expanded attribute to false
-    target.setAttribute("aria-expanded", false);
-  }
+// function enableNavigation() {
+//   const menuButton = document.querySelector("#global-nav-toggle");
+//   const subMenuToggles = document.querySelectorAll(".global-nav__split-button__toggle");
 
-  console.log("toggle");
-  });
-}
+//   console.log(subMenuToggles);
+  
+//   // when the main menu button is clicked:
+//   menuButton.addEventListener("click", (ev) => {
+//     let target = ev.target;
+//   // toggle the show class on the global-nav
+//   document.querySelector(".global-nav").classList.toggle("show");
+//   // check to see if ev.target is the button or something inside the button
+//   if (target.tagName != "BUTTON") {
+//     target = target.closest("button");
+//   }
+//   // check to see if we just opened or closed the menu
+//   if (document.querySelector(".global-nav").classList.contains("show")) {
+//     // if we opened it then set the aria-expanded attribute to true
+//     target.setAttribute("aria-expanded", true);
+//   } else {
+//     // if we closed it then set the aria-expanded attribute to false
+//     target.setAttribute("aria-expanded", false);
+//   }
+
+//   console.log("toggle");
+//   });
+
+//   subMenuToggles.forEach((toggle) => {
+//     //for each submenu toggle
+//     toggle.addEventListener("click", (ev) => {
+//       // find the closest li ancestor, then find the submenu inside of that li and toggle the show class
+//       ev.currentTarget.closest("li").querySelector(".global-nav__submenu").classList.toggle("show");
+//       // toggle the rotate class on the button icon that was clicked
+//       ev.currentTarget.querySelector(".icon").classList.toggle("rotate");
+//     });
+//   });
+// }
+
+// subMenuToggles.forEach((toggle) => {
+//   toggle.addEventListener("click", (ev) => {
+//     const submenu = ev.currentTarget.closest("li").querySelector(".global-nav__submenu");
+//     if (submenu) { // Check if the submenu exists
+//       submenu.classList.toggle("show");
+//       ev.currentTarget.querySelector(".icon").classList.toggle("rotate");
+//     } else {
+//       console.log("No submenu found for this toggle.");
+//     }
+//   });
+// });
+
 
 async function init() {
   const parkData = await getParkData();
@@ -49,6 +75,6 @@ async function init() {
   setHeaderFooter(parkData);
   setParkIntro(parkData);
   setParkInfoLinks(links);
-  enableNavigation();
+  // enableNavigation();
 }
 init();
